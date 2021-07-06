bbd79d6f78a071f33caabaffc7ab98a5759ce188
Taichi Kuranari <67308225+Taichi-Kura@users.noreply.github.com>
2021-07-06 11:47:33 +0900

    7/6の定例UITrace実装

---
+++ UITrace_7.6/UITrace_7.6/Boder.swift
@@ -0,0 +1,44 @@
+//
+//  Boder.swift
+//  UITrace_7.6
+//
+//  Created by 倉成汰一 on 2021/07/06.
+//
+
+import UIKit
+
+extension UIView {
+
+    // 枠線の色
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return layer.borderColor.map { UIColor(cgColor: $0) }
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    // 枠線のWidth
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    // 角丸設定
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
+
+
---
+++ UITrace_7.6/UITrace_7.6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITrace_7.6
+//
+//  Created by 倉成汰一 on 2021/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
